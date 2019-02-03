d5c76a2f6a2b159d70b7b3b18c35607d24921e27
Ryan Elfman <ryanelfman@gmail.com>
2019-02-03 11:10:08 -0800

    test

---
+++ 2019/2/3/a-simple-bid-ask-matching-algorithm-with-csharp/OrderBook.V1.cs
@@ -0,0 +1,14 @@
+﻿public class OrderBook
+{
+    public OrderBook()
+    {
+        // Always want a single limit so we can have less checks in the Ask and Bid methods.
+        Limits = new RankedSet<Limit>(new LimitPriceComparer()) { new Limit { Price = 1 * ScaleFactor } };
+        Subscriptions = new Dictionary<long, Subscription>();
+        Orders = new Dictionary<long, Order>();
+    }
+
+    private protected RankedSet<Limit> Limits { get; }
+    private protected IDictionary<long, Subscription> Subscriptions { get; }
+    private protected IDictionary<long, Order> Orders { get; }
+}
